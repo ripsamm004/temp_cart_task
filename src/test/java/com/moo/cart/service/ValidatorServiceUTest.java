6358ae06f3bcc7e8bcfb2b70261274d08e47c239
shiful <rikto.islam@gmail.com>
2018-05-28 14:44:57 +0100

    adding more complete one test

--- src/test/java/com/moo/cart/service/ValidatorServiceUTest.java
+++ src/test/java/com/moo/cart/service/ValidatorServiceUTest.java
@@ -2,6 +2,7 @@ package com.moo.cart.service;
 
 import com.moo.cart.api.ItemDTO;
 import com.moo.cart.api.exception.BadRequestException;
+import com.moo.cart.models.Item;
 import com.moo.cart.models.Product;
 import org.junit.Rule;
 import org.junit.Test;
@@ -11,10 +12,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Optional;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
@@ -50,8 +54,7 @@ public class ValidatorServiceUTest {
     }
 
     @Test
-    public void testIfGivenItemDTOHaveEmptyValuesThenThrowExceptions()
-            throws Exception {
+    public void testIfGivenItemDTOHaveEmptyValuesThenThrowExceptions() {
         ItemDTO itemDTO = new ItemDTO(null, 0);
         Optional<Product> mockProduct0 = Optional.ofNullable(null);
         when(productService.getProduct(anyString())).thenReturn(mockProduct0);
@@ -61,8 +64,7 @@ public class ValidatorServiceUTest {
     }
 
     @Test
-    public void testIfGivenItemDTOWithWrongCodeThenThrowExceptions()
-            throws Exception {
+    public void testIfGivenItemDTOWithWrongCodeThenThrowExceptions() {
         ItemDTO itemDTO = new ItemDTO("WRONG_CODE", 10);
         Optional<Product> mockProduct0 = Optional.ofNullable(null);
         when(productService.getProduct(anyString())).thenReturn(mockProduct0);
@@ -71,73 +73,47 @@ public class ValidatorServiceUTest {
         validator.validateItemDto(itemDTO);
     }
 
-    /*@Test
-    public void testIfUserNameIsNotValidThenExpectFalse() throws Exception {
-
-        String[] username = {"ripsamm004-", "testspecialchar £004", "test space", "r 333"};
-
-        assertThat(userValidator.validateUsername(username[0]), is(false));
-        assertThat(userValidator.validateUsername(username[1]), is(false));
-        assertThat(userValidator.validateUsername(username[2]), is(false));
-        assertThat(userValidator.validateUsername(username[3]), is(false));
+    @Test
+    public void testIfGivenCorrectItemDTOThenExpectItem() {
+        String code = "ABCD";
+        ItemDTO itemDTO = new ItemDTO(code, 10);
+        Product p0 = new Product(code, "Product-ABCD", 1.50d);
+        Optional<Product> mockProduct0 = Optional.ofNullable(p0);
+        when(productService.getProduct(code)).thenReturn(mockProduct0);
+        Item item = validator.validateItemDto(itemDTO);
+        assertEquals(item.getProduct(), p0);
+        assertEquals(item.getQuantity(), 10);
     }
-
 
     @Test
-    public void testIfUserDOBIsValidThenExpectTrue() throws Exception {
+    public void testIfItemDTOIsValidThenReturnItemWithProduct() throws Exception {
 
-        String[] dob = {"1917-03-02", "1917-12-02", "1917-02-02", "1917-03-02"};
-
-        assertThat(userValidator.validateDob(dob[0]), is(true));
-        assertThat(userValidator.validateDob(dob[1]), is(true));
-        assertThat(userValidator.validateDob(dob[2]), is(true));
-        assertThat(userValidator.validateDob(dob[3]), is(true));
-
+        String code = "ABCD";
+        ItemDTO itemDTO = new ItemDTO(code, 10);
+        Product p0 = new Product(code, "Product-ABCD", 1.50d);
+        Optional<Product> mockProduct0 = Optional.ofNullable(p0);
+        when(productService.getProduct(code)).thenReturn(mockProduct0);
+        Method method = Validator.class.getDeclaredMethod("dtoToItem", ItemDTO.class);
+        method.setAccessible(true);
+        Item item = (Item) method.invoke(validator, itemDTO);
+        assertEquals(item.getProduct(), p0);
+        assertEquals(item.getQuantity(), 10);
     }
 
+    /**
+     * Reflection method invocation throws InvocationTargetException exception if the method call throws exceptions
+     * Where method.invoke(validator, itemDTO) return the Optional<Product> as null so we expect InvocationTargetException
+     * @throws Exception
+     */
 
-    @Test
-    public void testIfUserDOBIsNotValidThenExpectFalse() throws Exception {
-
-        String[] dob = {"1917-03-02 09:02", "1917-20-12", "02-20-1917", "1917-03-02 12:01:04"};
-
-        assertThat(userValidator.validateDob(dob[0]), is(false));
-        assertThat(userValidator.validateDob(dob[1]), is(false));
-        assertThat(userValidator.validateDob(dob[2]), is(false));
-        assertThat(userValidator.validateDob(dob[3]), is(false));
+    @Test(expected = InvocationTargetException.class)
+    public void testIfItemDTOIsInValidThenThrowExceptions() throws Exception  {
+        String code = "ABCD";
+        ItemDTO itemDTO = new ItemDTO(code, 10);
+        Optional<Product> mockProduct0 = Optional.ofNullable(null);
+        when(productService.getProduct(code)).thenReturn(mockProduct0);
+        Method method = Validator.class.getDeclaredMethod("dtoToItem", ItemDTO.class);
+        method.setAccessible(true);
+        method.invoke(validator, itemDTO);
     }
-
-    @Test(expected = BadRequestException.class)
-    public void testIfGivenUserDTONameIsNotValidThenThrowExceptions() throws Exception {
-        UserDTO userDTO = new UserDTO("usern ame22", "443908Rr", "1986-05-26", "ssn110");
-        userValidator.validateUser(userDTO);
-    }
-
-    @Test(expected = BadRequestException.class)
-    public void testIfGivenUserDTOPasswordIsNotValidThenThrowExceptions() throws Exception {
-        UserDTO userDTO = new UserDTO("username22", "44444", "1986-05-26", "ssn110");
-        userValidator.validateUser(userDTO);
-    }
-
-    @Test(expected = BadRequestException.class)
-    public void testIfGivenUserDTODOBIsNotValidThenThrowExceptions() throws Exception {
-        UserDTO userDTO = new UserDTO("username22", "44444rR", "1986-30-02", "ssn110");
-        userValidator.validateUser(userDTO);
-    }
-
-    @Test(expected = BadRequestException.class)
-    public void testIfGivenUserDTOHaveEmptyValuesThenThrowExceptions()
-            throws Exception {
-
-        UserDTO userDTO = new UserDTO("", "", "", "");
-        userValidator.validateUser(userDTO);
-    }
-
-    @Test(expected = BadRequestException.class)
-    public void testIfGivenUserDTOHaveNullValuesThenThrowExceptions()
-            throws Exception {
-
-        UserDTO userDTO = new UserDTO(null, null, null, null);
-        userValidator.validateUser(userDTO);
-    }*/
 }
